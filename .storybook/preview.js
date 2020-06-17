e0dcc5d2697fd785791cf34515075ab789747afe
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-06-18 08:09:53 +0900

    addon-backgrounds

---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+export const parameters = {
+  backgrounds: {
+    default: 'light',
+    values: [
+      { name: 'white', value: '#ffffff' },
+      { name: 'light', value: '#eeeeee' },
+      { name: 'dark', value: '#222222' },
+    ],
+  },
+};
